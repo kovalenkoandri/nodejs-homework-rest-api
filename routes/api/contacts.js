da39a9923c5cdc90a1a666591ed9d67e02a83097
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-12 17:44:35 +0200

    listContacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,14 @@
 const express = require('express')
-
+const {
+  listContacts,
+  // getContactById,
+  // addContact,
+  // removeContact,
+} = require('../../models/contacts.js');
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(await listContacts());
 })
 
 router.get('/:contactId', async (req, res, next) => {
