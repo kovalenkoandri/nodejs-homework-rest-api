83be78de4ae3b2a9e5bcfb91af2436db180ac310
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-12 22:17:49 +0200

    addContact

--- models/contacts.js
+++ models/contacts.js
@@ -1,7 +1,7 @@
-// const fs = require('fs/promises')
-// const fs = require('fs').promises;
 const contacts = require('./contacts.json');
-
+const { v4 } = require('uuid');
+const fs = require('fs/promises');
+const path = require('path');
 const listContacts = async () => contacts;
 
 const getContactById = async (contactId = 1) => {
@@ -15,7 +15,14 @@ const getContactById = async (contactId = 1) => {
 
 const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  const products = await listContacts();
+  const newProduct = { id: v4(),...body };
+  products.push(newProduct);
+  const filePath = path.join(__dirname, 'contacts.json');
+  await fs.writeFile(filePath, JSON.stringify(products));
+  return newProduct;
+};
 
 const updateContact = async (contactId, body) => {};
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,26 @@
-const express = require('express')
+const express = require('express');
 const {
   listContacts,
   getContactById,
-  // addContact,
+  addContact,
   // removeContact,
 } = require('../../models/contacts.js');
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-   try {
-     const products = await listContacts();
-     res.json({
-       status: 'success',
-       code: 200,
-       data: {
-         result: products,
-       },
-     });
-   } catch (error) {
-     next(error);
-   }
-})
+  try {
+    const products = await listContacts();
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result: products,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
   try {
@@ -41,18 +41,35 @@ router.get('/:contactId', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    //  const { error } = productSchema.validate(req.body);
+    //  if (error) {
+    //    const err = new Error(error.message);
+    //    err.status = 400;
+    //    throw err;
+    //  }
+    const result = await addContact(req.body);
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
