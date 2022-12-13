5259c1b3c6ab7afe67239e6eaab23d5d7d45e16e
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-13 23:52:59 +0200

    comments

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const app = require('./app')
+const app = require('./app');
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
-   
