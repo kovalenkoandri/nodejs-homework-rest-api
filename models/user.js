102735b036a2485e1afc657a6bdb16090971f72f
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-09 01:53:24 +0200

    rm duplicate

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
-const bcrypt = require('bcryptjs');
 
 const userSchema = Schema(
   {
@@ -29,15 +28,6 @@ const userSchema = Schema(
   },
   { versionKey: false },
 );
-
-
-// userSchema.methods.setPassword = function (password) {
-//   this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-// };
-
-// userSchema.methods.comparePassword = function (password) {
-//   return bcrypt.compareSync(password, this.password);
-// };
 
 const joiRegisterSchema = Joi.object({
   name: Joi.string().required(),
