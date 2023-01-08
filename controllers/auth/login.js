e18f97fca0a1c7b174dac1c23bd1889447e95b7b
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-09 01:57:39 +0200

    npm i jsonwebtoken

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,6 +1,8 @@
 const { User } = require('../../models');
 const { Unauthorized } = require('http-erros');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -11,7 +13,18 @@ const login = async (req, res) => {
   const passCompare = bcrypt.compareSync(password, user.password);
   if (!passCompare) {
     throw new Unauthorized('Password wrong');
-  }
+    }
+    const payload = {
+      id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        token,
+      },
+    });
 };
 
 module.exports = login;
