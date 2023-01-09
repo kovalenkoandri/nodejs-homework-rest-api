3d037b56dc8fe8d61d9f36b8cee8ceec58583428
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-09 14:53:12 +0200

    http-errors

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,5 @@
 const { User } = require('../../models');
-const { Unauthorized } = require('http-erros');
+const { Unauthorized } = require('http-errors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env;
