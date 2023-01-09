28e0b59e990dc84af061998d38242869b19c4841
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-09 14:35:20 +0200

    pagination

--- models/contacts/listContacts.js
+++ models/contacts/listContacts.js
@@ -1,6 +1,8 @@
 const { Сontact } = require('../category');
 const listContacts = async (req) => {
     const { _id } = req.user;
-    return Сontact.find({ owner: _id });
+    const { page = 1, limit = 20 } = req.query;
+    const { skip } = (page - 1) * limit;
+    return Сontact.find({ owner: _id }, '', {skip, limit: Number(limit)});
 };
 module.exports = listContacts;
