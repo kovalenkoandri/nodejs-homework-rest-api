7b16876945178b3bfc9d5d3406705792fc29c88c
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-11 06:25:46 +0200

    listContacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,21 +2,17 @@ const { Contact } = require('../../models');
 const { success200 } = require('../../helpers');
 
 const listContacts = async (req, res) => {
-  console.log(Contact.contact);
   const { _id } = req.user;
-  console.log(_id);
-  const { page = 1, limit = 2, favorite = true } = req.query;
+  const { page = 1, limit = 7, favorite = true } = req.query;
   const skip = (page - 1) * limit;
-  const data = await Contact.find({ owner: _id }).populate(
-    'owner',
-    '_id name email',
-  );;
-  // const data = await Contact.find().populate('owner', '_id name email');
-  // const data = await Contact.find({ owner: _id }, '', {
-  //   skip,
-  //   limit: Number(limit),
-  //   favorite,
-  // }).populate('owner', '_id name email');
+  const data = await Contact.find(
+    { owner: _id, favorite },
+    'email phone favorite',
+    {
+      skip,
+      limit: Number(limit),
+    },
+  ).populate('owner', '_id email subscription');
   success200(res, data);
 };
 
