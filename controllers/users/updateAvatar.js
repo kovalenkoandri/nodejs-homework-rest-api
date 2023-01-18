fd2f1b50d7660adc47d2423c8c8bc26117e1b550
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-01-18 14:49:51 +0200

    imagekit done

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -2,7 +2,7 @@ const { User } = require('../../models');
 const fsSync = require('fs');
 // const fs = require('fs/promises');
 const Jimp = require('jimp');
-// const sharp = require('sharp');
+const sharp = require('sharp');
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 const ImageKit = require('imagekit');
@@ -14,44 +14,41 @@ const imagekit = new ImageKit({
   publicKey: 'public_ORBWILMeHfwdACTacb9STD4WE7A=',
   privateKey: process.env.IMAGE_KIT_CRED,
   urlEndpoint: 'https://ik.imagekit.io/xtrtlw85x/',
-  // urlEndpoint: 'https://ik.imagekit.io/your_imagekit_id/',
 });
-// const imageURL = imagekit.url({
-//   path: '/default-image.jpg',
-//   src: 'https://ik.imagekit.io/your_imagekit_id/xtrtlw85x/endpoint/myimage.png',
-//   transformation: [
-//     {
-//       height: '300',
-//       width: '400',
-//     },
-//   ],
-// });
+imagekit.url({
+  useUniqueFileName: false,
+});
+
 // const tempDir = 'temp/rm';
-    // Promise.resolve(fs.mkdir(tempDir, { recursive: true }));
+// Promise.resolve(fs.mkdir(tempDir, { recursive: true }));
+const imageToBase64 = require('image-to-base64');
+
 const updateAvatar = async (req, res) => {
   const tempUpload = req.file.path;
 
   try {
     let avatarURL;
+    // let base64String;
     await Jimp.read(tempUpload).then((img) => {
       img.resize(250, 250).write('temp/rm/3.jpg');
     });
-    // await sharp('temp/2.png').resize(320, 240).toFile('temp/3.png');
-    const file = fsSync.readFileSync(tempUpload);
-    const blob = Buffer.from(file);
-    // const file = fs.readFileSync('/some/place/image.png');
-    // const base64String = Buffer.from(file).toString('base64');
+    // await sharp(tempUpload).resize(320, 240).toFile('temp/rm/3.jpg');
+    // const file = fsSync.readFileSync(tempUpload);
+    // const blob = Buffer.from(file);
+    // await imageToBase64('temp/rm/3.jpg') // Path to the image
+    //   .then((response) => {
+    //     // console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
+    //     base64String = response;
+    //   })
+    //   .catch((error) => {
+    //     console.log(error); // Logs an error if there was one
+    //   });
+    const file = fsSync.readFileSync('temp/rm/3.jpg');
+    const base64String = Buffer.from(file).toString('base64');
     await imagekit
       .upload({
-        file: blob,
+        file: base64String,
         fileName: 'myimage.jpg',
-        // extensions: [
-        //   {
-        //     name: 'google-auto-tagging',
-        //     maxTags: 5,
-        //     minConfidence: 95,
-        //   },
-        // ],
       })
       .then((response) => {
         console.log(response);
@@ -83,12 +80,12 @@ const updateAvatar = async (req, res) => {
   } finally {
     const files = [
       'temp/rm/2.jpg',
-      'temp/rm/3.jpg',
+      // 'temp/rm/3.jpg',
       'temp/rm/iceberg.jpg',
       'temp/rm/default-image.jpg',
     ];
     files.forEach((path) => fsSync.existsSync(path) && fsSync.unlinkSync(path));
-    
+
     // await fs.rm(tempDir, { recursive: true, force: true }, (err) => {
     //   if (err) {
     //     throw err;
